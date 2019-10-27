ce6cf3584bc394f61a40044978e2185e95f41c24
mrech <rivato.morena@gmail.com>
2019-10-27 18:42:22 +0100

    remove stopwords

--- tokenizer.py
+++ tokenizer.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import Tokenizer
@@ -23,6 +24,28 @@ for item in data:
     sentences.append(item['headline'])
     labels.append(item['is_sarcastic'])
     urls.append(item['article_link'])
+
+#Stopwords list from https://github.com/Yoast/YoastSEO.js/blob/develop/src/config/stopwords.js
+stopwords = ["a", "about", "above", "after", "again", "against", "all", "am", "an", "and",
+             "any", "are", "as", "at", "be", "because", "been", "before", "being", "below",
+             "between", "both", "but", "by", "could", "did", "do", "does", "doing", "down",
+             "during", "each", "few", "for", "from", "further", "had", "has", "have",
+             "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers",
+             "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm",
+             "i've", "if", "in", "into", "is", "it", "it's", "its", "itself", "let's", "me",
+             "more", "most", "my", "myself", "nor", "of", "on", "once", "only", "or", "other",
+             "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "she", "she'd",
+             "she'll", "she's", "should", "so", "some", "such", "than", "that", "that's", "the",
+             "their", "theirs", "them", "themselves", "then", "there", "there's", "these",
+             "they", "they'd", "they'll", "they're", "they've", "this", "those", "through",
+             "to", "too", "under", "until", "up", "very", "was", "we", "we'd", "we'll",
+             "we're", "we've", "were", "what", "what's", "when", "when's", "where",
+             "where's", "which", "while", "who", "who's", "whom", "why", "why's",
+             "with", "would", "you", "you'd", "you'll", "you're", "you've", "your",
+             "yours", "yourself", "yourselves"]
+
+for num, sentence in enumerate(sentences):
+    sentences[num] = ' '.join([w for w in sentence.strip().split() if not w in stopwords])
 
 # out of vocabulary words token <OOV>
 # Tokenizer object used to tokenize sentences
